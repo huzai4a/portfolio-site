eab1329ecf098bfa38ae2465d0a019b23d78149d
Huzaifa Syed <huzaifa_syed78@hotmail.com>
2024-10-14 16:53:59 -0400

    removed filler code

--- app/page.tsx
+++ app/page.tsx
@@ -28,53 +28,6 @@ export default function Home() {
         <Experience />
         <Projects />
         
-        <div className="max-w-7xl w-full">
-            <div className="flex space-x-4">
-              <a href="#section1" className="text-white">Section 1</a>
-              <a href="#section2" className="text-white">Section 2</a>
-              <a href="#section3" className="text-white">Section 3</a>
-            </div>
-
-        <div className="pt-20">
-          <section id="section1" className="h-screen">
-            <h2 className="text-white text-4xl mb-4">Section 1: Welcome</h2>
-            <p className="text-gray-300">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet,
-              nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula
-              venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.
-            </p>
-          </section>
-
-          <section id="section2" className="h-screen">
-            <h2 className="text-white text-4xl mb-4">Section 2: Features</h2>
-            <p className="text-gray-300">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            </p>
-          </section>
-
-          <section id="section3" className="h-screen">
-            <h2 className="text-white text-4xl mb-4">Section 3: Contact</h2>
-            <p className="text-gray-300">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-            </p>
-          </section>
-          
-          <section className="h-screen">
-            <h2 className="text-white text-4xl mb-4">More Content</h2>
-            <p className="text-gray-300">
-              This is additional content to make the page scrollable. Keep adding more content as necessary.
-            </p>
-            <p className="text-gray-300">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet,
-              nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula
-              venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.
-            </p>
-          </section>
-        </div>
-      </div>
-        
     </div>
   );
 }
